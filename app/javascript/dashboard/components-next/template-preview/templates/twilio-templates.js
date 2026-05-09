f76615e5ebf3ecdb7205766a814739ca1285230e
genius <niteshgator@gmail.com>
2026-05-09 13:50:58 -0500

    rebrand chatwoot to patra

--- app/javascript/dashboard/components-next/template-preview/templates/twilio-templates.js
+++ app/javascript/dashboard/components-next/template-preview/templates/twilio-templates.js
@@ -129,18 +129,18 @@ export const twilioTemplates = [
     template_type: 'quick_reply',
   },
   {
-    body: 'What type of Chatwoot installation are you using? Select "Chatwoot Cloud" if you are using app.chatwoot.com, otherwise select "Self-hosted Chatwoot".',
+    body: 'What type of Chatwoot installation are you using? Select "Patra Cloud" if you are using app.chatwoot.com, otherwise select "Self-hosted Patra".',
     types: {
       'twilio/quick-reply': {
-        body: 'What type of Chatwoot installation are you using? Select "Chatwoot Cloud" if you are using app.chatwoot.com, otherwise select "Self-hosted Chatwoot".',
+        body: 'What type of Chatwoot installation are you using? Select "Patra Cloud" if you are using app.chatwoot.com, otherwise select "Self-hosted Patra".',
         actions: [
           {
-            id: 'Chatwoot Cloud_payload',
-            title: 'Chatwoot Cloud',
+            id: 'Patra Cloud_payload',
+            title: 'Patra Cloud',
           },
           {
-            id: 'Self-hosted Chatwoot_payload',
-            title: 'Self-hosted Chatwoot',
+            id: 'Self-hosted Patra_payload',
+            title: 'Self-hosted Patra',
           },
         ],
       },
