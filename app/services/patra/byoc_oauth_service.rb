d6134b93e47df06e08d78cf446e4de129687b8db
genius <niteshgator@gmail.com>
2026-05-22 15:10:30 -0500

    debug(patra): add [PatraBYOC-debug] logging to track /me/accounts response + per-page upsert

--- app/services/patra/byoc_oauth_service.rb
+++ app/services/patra/byoc_oauth_service.rb
@@ -22,8 +22,16 @@ module Patra
       raise Error, 'Could not fetch Facebook user profile' if user_profile.blank?
 
       pages = fetch_user_pages!(long_lived)
+      Rails.logger.info(
+        "[PatraBYOC-debug] /me/accounts returned #{pages.length} pages: " \
+        "#{pages.map { |p| { id: p[:id], name: p[:name] } }.inspect}"
+      )
       identity = upsert_facebook_identity!(user_profile, long_lived)
       created_channels = pages.map { |page| upsert_byoc_channel!(page, identity) }
+      Rails.logger.info(
+        "[PatraBYOC-debug] complete! returning #{created_channels.compact.length} " \
+        "created channels of #{pages.length} pages received"
+      )
 
       { identity: identity, pages: created_channels }
     end
@@ -70,54 +78,69 @@ module Patra
     def upsert_byoc_channel!(page, identity)
       page_id = page[:id].to_s
       page_name = page[:name].presence || "Facebook #{page_id}"
-
-      legacy_channel = @account.facebook_pages.find_by(page_id: page_id)
-      if legacy_channel&.inbox
-        return {
-          id: page_id,
-          name: legacy_channel.inbox.name,
-          action: 'already_connected_legacy'
-        }
-      end
-
-      user_token = identity.user_access_token
-      page_token = page[:access_token].presence ||
-                   Facebook::PatraGraphService.long_lived_page_access_token(page_id, user_token)
-      Facebook::PatraGraphService.subscribe_page_webhook(
-        page_id,
-        page_token,
-        app_id: @app_id,
-        app_secret: @app_secret
+      Rails.logger.info(
+        "[PatraBYOC-debug] upserting page id=#{page_id} name=#{page[:name].inspect}"
       )
 
-      channel_attrs = byoc_channel_attributes(page_id, page_token, user_token)
-      existing = fb_bridge_channel_for_page(page_id)
+      begin
+        legacy_channel = @account.facebook_pages.find_by(page_id: page_id)
+        if legacy_channel&.inbox
+          Rails.logger.info(
+            "[PatraBYOC-debug] upserted page id=#{page_id} -> inbox=#{legacy_channel.inbox.id}"
+          )
+          return {
+            id: page_id,
+            name: legacy_channel.inbox.name,
+            action: 'already_connected_legacy'
+          }
+        end
 
-      if existing
-        attrs = (existing.additional_attributes || {}).stringify_keys.merge(channel_attrs)
-        existing.update!(
-          additional_attributes: attrs,
-          facebook_identity_id: identity.id
+        user_token = identity.user_access_token
+        page_token = page[:access_token].presence ||
+                     Facebook::PatraGraphService.long_lived_page_access_token(page_id, user_token)
+        Facebook::PatraGraphService.subscribe_page_webhook(
+          page_id,
+          page_token,
+          app_id: @app_id,
+          app_secret: @app_secret
         )
-        inbox = existing.inbox
-        action = 'updated'
-      else
-        channel = @account.api_channels.create!(
-          webhook_url: '',
-          hmac_mandatory: false,
-          facebook_identity_id: identity.id,
-          additional_attributes: channel_attrs
+
+        channel_attrs = byoc_channel_attributes(page_id, page_token, user_token)
+        existing = fb_bridge_channel_for_page(page_id)
+
+        if existing
+          attrs = (existing.additional_attributes || {}).stringify_keys.merge(channel_attrs)
+          existing.update!(
+            additional_attributes: attrs,
+            facebook_identity_id: identity.id
+          )
+          inbox = existing.inbox
+          action = 'updated'
+        else
+          channel = @account.api_channels.create!(
+            webhook_url: '',
+            hmac_mandatory: false,
+            facebook_identity_id: identity.id,
+            additional_attributes: channel_attrs
+          )
+          inbox = @account.inboxes.create!(name: page_name, channel: channel)
+          add_inbox_members!(inbox)
+          action = 'created'
+        end
+
+        Rails.logger.info("[PatraBYOC-debug] upserted page id=#{page_id} -> inbox=#{inbox.id}")
+
+        {
+          id: page_id,
+          name: inbox.name,
+          action: action
+        }
+      rescue StandardError => e
+        Rails.logger.error(
+          "[PatraBYOC-debug] upsert FAILED page id=#{page_id}: #{e.class} #{e.message}"
         )
-        inbox = @account.inboxes.create!(name: page_name, channel: channel)
-        add_inbox_members!(inbox)
-        action = 'created'
+        raise
       end
-
-      {
-        id: page_id,
-        name: inbox.name,
-        action: action
-      }
     end
 
     def byoc_channel_attributes(page_id, page_token, user_token)
