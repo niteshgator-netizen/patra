121a1872b10b363d97f5cce79a55ed930576fcfa
genius <niteshgator@gmail.com>
2026-05-24 17:57:54 -0500

    fix: add player_bonus inflection rule so Account#player_bonuses resolves

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,7 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular 'player_bonus', 'player_bonuses'
+end
