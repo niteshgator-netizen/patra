ceb94540a096b75f4b2836d9ce63113d83fc728d
genius <niteshgator@gmail.com>
2026-06-09 13:40:17 -0500

    fix: add missing Current.ip_address accessor (audit logging silently failed in background/job context)

--- lib/current.rb
+++ lib/current.rb
@@ -4,6 +4,7 @@ module Current
   thread_mattr_accessor :account_user
   thread_mattr_accessor :executed_by
   thread_mattr_accessor :contact
+  thread_mattr_accessor :ip_address
 
   def self.reset
     Current.user = nil
@@ -11,5 +12,6 @@ module Current
     Current.account_user = nil
     Current.executed_by = nil
     Current.contact = nil
+    Current.ip_address = nil
   end
 end
